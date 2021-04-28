5bf5d0cccf05765570266bee4b860466059ade83
Jan Wawszczak <janekw23@gmail.com>
2021-04-29 00:05:34 +0200

    Mockup site is correctly loaded

--- mysite/prover/views.py
+++ mysite/prover/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the prover index.")
+    template = loader.get_template('prover/index.html')
+    return HttpResponse(template.render(None, request))
