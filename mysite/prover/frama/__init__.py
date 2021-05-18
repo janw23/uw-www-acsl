80065c7e5bab5b1f621007dcea353e2ca395ee0c
Jan Wawszczak <janekw23@gmail.com>
2021-05-18 14:01:26 +0200

    Add warning message

--- mysite/prover/frama/__init__.py
+++ mysite/prover/frama/__init__.py
@@ -1,13 +1,18 @@
 import pathlib
 from subprocess import run
+import sys
 
 FRAMA_ROOT = pathlib.Path(__file__).resolve().parent
+
+
+def _print_warning(msg):
+    print('WARNING:', msg, file=sys.stderr)
 
 
 def wp_print(fp):
     wp_print_script = FRAMA_ROOT / pathlib.Path('remote_wp_print.sh')
     assert wp_print_script.is_file()
-    cmd = f'{wp_print_script} {fp}'
+    _print_warning('Frama runs on students via SSH')
     result = run(f'{wp_print_script} {fp}',
                  capture_output=True, text=True, shell=True)
     return result.stdout.split('\n'), result.stderr.split('\n')
