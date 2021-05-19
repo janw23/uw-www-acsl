7fb6b4725966b9fb29e98745444a203e800efcbb
Jan Wawszczak <janekw23@gmail.com>
2021-05-19 13:47:56 +0200

    Add local script version

--- mysite/prover/frama/remote_wp_print.sh
+++ mysite/prover/frama/remote_wp_print.sh
@@ -6,3 +6,6 @@ base=$(basename "$fp")
 scp -q "${fp}" students:~/frama_service/
 ssh students "cd ~/frama_service && frama-c -wp -wp-print ${base}"
 ssh students "rm ~/frama_service/${base}"
+
+# uncomment this and comment above for local version
+# frama-c -wp -wp-print $fp
